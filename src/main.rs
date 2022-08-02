9299cccc286bfdaea6e470fc938a8edd6642f49f
Shane McDonough <shanemcdman@gmail.com>
2022-08-01 23:44:16 -0700

    Improve fsm and write another test

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,7 @@ impl<T: Iterator<Item = u8> + 'static> From<T> for TokenIterator {
 }
 
 impl Iterator for TokenIterator {
-    type Item = Result<Token, char>;
+    type Item = Result<Token, String>;
 
     fn next(&mut self) -> Option<Self::Item> {
         let mut state = State::Start;
@@ -46,36 +46,31 @@ impl Iterator for TokenIterator {
         while let Some(byte) = self.ungotten.take().or_else(|| self.it.next()) {
             let ch = byte as char;
             match (state, ch) {
-                (State::Start, ' ' | '\n' | '\t' | '\r') => (),
+                (State::Start, ' ' | '\n' | '\t' | '\r') => continue,
                 (State::Start, '(') => return Some(Ok(Token::LParen)),
                 (State::Start, ')') => return Some(Ok(Token::RParen)),
-                (State::Start, 'a'..='z' | 'A'..='Z' | '_') => {
-                    state = State::Ident;
-                    partial.push(ch);
+                (State::Start, 'a'..='z' | 'A'..='Z' | '_') => state = State::Ident,
+                (State::Start | State::Int | State::Float, '0'..='9') => if state == State::Start {
+                    state = State::Int;
                 }
-                (State::Start | State::Int | State::Float, '0'..='9') => {
-                    if state == State::Start {
-                        state = State::Int;
-                    }
-                    partial.push(ch);
-                }
-                (State::Start | State::Int, '.') => {
-                    state = State::Float;
-                    partial.push(ch);
-                }
-                (State::Ident, 'a'..='z' | 'A'..='Z' | '_' | '0'..='9') => partial.push(ch),
+                (State::Start | State::Int, '.') => state = State::Float,
+                (State::Int | State::Float, 'a'..='z' | 'A'..='Z') => state = State::Error,
+                (State::Ident | State::Error, 'a'..='z' | 'A'..='Z' | '0'..='9' | '_') => (),
                 _ => {
                     if state != State::Start {
+                        assert!(self.ungotten.is_none());
                         self.ungotten = Some(byte);
                     }
                     return match state {
-                        State::Start => Some(Err(ch)),
+                        State::Start => Some(Err(ch.to_string())),
                         State::Ident => Some(Ok(Token::Ident(partial))),
                         State::Int => Some(Ok(Token::Int(i64::from_str(&partial).unwrap()))),
                         State::Float => Some(Ok(Token::Float(f64::from_str(&partial).unwrap()))),
+                        State::Error => Some(Err(partial)),
                     };
                 }
             }
+            partial.push(ch)
         }
         None
     }
@@ -87,6 +82,7 @@ enum State {
     Ident,
     Int,
     Float,
+    Error,
 }
 
 #[cfg(test)]
@@ -105,7 +101,7 @@ mod tests {
         assert_eq!(it.next(), Some(Ok(Token::Ident("abc".to_string()))));
         assert_eq!(it.next(), Some(Ok(Token::Int(123))));
         assert_eq!(it.next(), Some(Ok(Token::Float(1.3))));
-        assert_eq!(it.next(), Some(Err('=')));
+        assert_eq!(it.next(), Some(Err("=".to_string())));
         assert_eq!(it.next(), Some(Ok(Token::RParen)));
         assert_eq!(it.next(), None);
         it = TokenIterator::from_str("(xyz()");
@@ -114,6 +110,16 @@ mod tests {
         assert_eq!(it.next(), Some(Ok(Token::LParen)));
         assert_eq!(it.next(), Some(Ok(Token::RParen)));
         assert_eq!(it.next(), None);
+    }
+
+    #[test]
+    fn letters_after_numbers_test() {
+        let mut it = TokenIterator::from_str("(123 123abc abc)");
+        assert_eq!(it.next(), Some(Ok(Token::LParen)));
+        assert_eq!(it.next(), Some(Ok(Token::Int(123))));
+        assert_eq!(it.next(), Some(Err("123abc".to_string())));
+        assert_eq!(it.next(), Some(Ok(Token::Ident("abc".to_string()))));
+        assert_eq!(it.next(), Some(Ok(Token::RParen)));
     }
 }
 
